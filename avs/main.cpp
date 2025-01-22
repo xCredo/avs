180dc6375a52c25078f21490f7ca919759d4c204
xCredo <94628584+xCredo@users.noreply.github.com>
2025-01-23 00:15:30 +0700

    Delete avs directory

--- avs/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello from Jenkins!" << std::endl;
-    std::cout << "Hello Jenkins from Jenkins!" << std::endl;
-    std::cout << "Hello Jenkins from Jenkins!" << std::endl;
-    std::cout << "Hello Jenkins from Jenkins!" << std::endl;
-    return 0;
-}
