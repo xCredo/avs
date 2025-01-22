d914597e275783d3ed205165a09f4bb0228b94b0
xCredo <94628584+xCredo@users.noreply.github.com>
2025-01-23 00:14:33 +0700

    Add files via upload

---
+++ avs/dockerfile
@@ -0,0 +1,5 @@
+FROM gcc:latest
+WORKDIR /usr/src/app
+COPY . .
+RUN g++ -o main main.cpp
+CMD ["./main"]
---
+++ avs/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from Jenkins!" << std::endl;
+    std::cout << "Hello Jenkins from Jenkins!" << std::endl;
+    std::cout << "Hello Jenkins from Jenkins!" << std::endl;
+    std::cout << "Hello Jenkins from Jenkins!" << std::endl;
+    return 0;
+}
